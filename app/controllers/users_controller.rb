feadfe03b421a7079de563d2e3b6b8c44586d3e7
Daniel Mueller <danielpetermueller@gmail.com>
2021-09-17 15:10:14 -0400

    cors and user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json:user
+        else record_not_found
+        end
+    end
+
+    def create
+        newUser = User.create(user_params)
+        if newUser.valid?
+            render json: newUser, status: :created
+        else
+            render json: {errors: newUser.errors.full_messages}, status: :unprocessable_entity
+        end 
+    end
+
+    private 
+
+    def record_not_found
+        render json: {error: "User not found"}, status: :not_found
+    end 
+
+    def user_params
+        params.permit(:username, :password, :acct_type, :total_points)
+    end
+    
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :individual_orders
-  resources :orders
-  resources :user_groups
-  resources :groups
-  resources :users
+  resources :individual_orders, only: [:index, :show, :create, :update, :destroy]
+  resources :orders, only: [:index, :show, :create, :update, :destroy]
+  resources :user_groups, only: [:index, :show, :create, :destroy]
+  resources :groups, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
